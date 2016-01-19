9127a1219da94422499a19061bf65012f8391250
Adam Jones <adds68@me.com>
2016-01-19 20:40:52 +0000

    started from chapter 4 base project to save time repeating code

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-/* Controlelrs */
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-
-/* Location Routes */
+/* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
-/* Other Routes */
+/* Other pages */
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
